7fa7168a7416d8be419b1cdf32d012604e2bbd2b
miguelmarttins <miguel.martins1030@gmail.com>
2024-08-23 14:47:07 -0300

    Criação da entidade para Livro

--- core/models/autor.py
+++ core/models/autor.py
@@ -6,3 +6,8 @@ class Autor(models.Model):
 
     def __str__(self):
         return f"(#{self.id}) {self.nome}"
+    
+    class Meta: 
+        verbose_name: "Autor"
+        verbose_name_plural: "Autores"
+        
---
+++ core/views/livro.py
@@ -0,0 +1,9 @@
+from rest_framework.viewsets import ModelViewSet
+
+from core.models import Categoria, Livro
+from core.serializers import  LivroSerializer
+
+
+class LivroViewSet(ModelViewSet):
+    queryset = Livro.objects.all()
+    serializer_class = LivroSerializer
